3f2efb4f4edf77f860f4ab370800246d331212a5
timmy-quinn <tquinn5301@gmail.com>
2024-11-12 23:50:05 -0500

    Used overlay file for gpio scanning

--- 01_FW/src/keys.c
+++ 01_FW/src/keys.c
@@ -4,7 +4,6 @@
 #include <hal/nrf_gpio.h>
 #include "keys.h"
 
-
 /* 
 * NRF dongle pins
 * side 1 
@@ -42,94 +41,103 @@ typedef struct pin {
 
 #define KEY_MATRIX_OUT_COUNT 8
 #define KEY_MATRIX_IN_COUNT 7
-#define SLEEP_TIME_MS 2
+#define SLEEP_TIME_MS 2 
 
-gpioPin_t keyMatrixOut[KEY_MATRIX_OUT_COUNT];
-gpioPin_t keyMatrixIn[KEY_MATRIX_IN_COUNT];
-
+static const struct gpio_dt_spec *keyMatrixOut[KEY_MATRIX_OUT_COUNT];
+static const struct gpio_dt_spec *keyMatrixIn[KEY_MATRIX_IN_COUNT];
+static const struct gpio_dt_spec *testLED; 
 uint8_t scanCodeMatrix[KEY_MATRIX_OUT_COUNT][KEY_MATRIX_IN_COUNT]; 
 
+#define EXT_LED_0        DT_NODELABEL(ledx0)
+#define EXT_LED_1        DT_NODELABEL(ledx1)
+#define EXT_LED_2        DT_NODELABEL(ledx2)
+#define EXT_LED_3        DT_NODELABEL(ledx3)
+#define EXT_LED_4        DT_NODELABEL(ledx4)
+#define EXT_LED_5        DT_NODELABEL(ledx5)
+#define EXT_LED_6        DT_NODELABEL(ledx6)
+#define EXT_LED_7        DT_NODELABEL(ledx7)
 
+#define TEST_LED        DT_NODELABEL(testled)
+
+#define BTN_MATRIX_0    DT_NODELABEL(btnmtrx0)
+#define BTN_MATRIX_1    DT_NODELABEL(btnmtrx1)
+#define BTN_MATRIX_2    DT_NODELABEL(btnmtrx2)
+#define BTN_MATRIX_3    DT_NODELABEL(btnmtrx3)
+#define BTN_MATRIX_4    DT_NODELABEL(btnmtrx4)
+#define BTN_MATRIX_5    DT_NODELABEL(btnmtrx5)
+#define BTN_MATRIX_6    DT_NODELABEL(btnmtrx6)
 
 void initKeyMatrix() {
-    const struct device *gpio0_dev = device_get_binding(GPIO_NAME_0);
-    const struct device *gpio1_dev = device_get_binding(GPIO_NAME_1);
+    static const struct gpio_dt_spec ledx0_spec = GPIO_DT_SPEC_GET(EXT_LED_0, gpios);
+    static const struct gpio_dt_spec ledx1_spec = GPIO_DT_SPEC_GET(EXT_LED_1, gpios);
+    static const struct gpio_dt_spec ledx2_spec = GPIO_DT_SPEC_GET(EXT_LED_2, gpios);
+    static const struct gpio_dt_spec ledx3_spec = GPIO_DT_SPEC_GET(EXT_LED_3, gpios);
+    static const struct gpio_dt_spec ledx4_spec = GPIO_DT_SPEC_GET(EXT_LED_4, gpios);
+    static const struct gpio_dt_spec ledx5_spec = GPIO_DT_SPEC_GET(EXT_LED_5, gpios);
+    static const struct gpio_dt_spec ledx6_spec = GPIO_DT_SPEC_GET(EXT_LED_6, gpios);
+    static const struct gpio_dt_spec ledx7_spec = GPIO_DT_SPEC_GET(EXT_LED_7, gpios);
 
-// GPIO outputs
-    keyMatrixOut[0].device = gpio0_dev;
-    keyMatrixOut[0].pinNum = 0;
+    static const struct gpio_dt_spec testLed_spec = GPIO_DT_SPEC_GET(TEST_LED, gpios);
 
-    keyMatrixOut[1].device = gpio0_dev;
-    keyMatrixOut[1].pinNum = 1;
+    static const struct gpio_dt_spec btnMtrx0_spec = GPIO_DT_SPEC_GET(BTN_MATRIX_0, gpios);  
+    static const struct gpio_dt_spec btnMtrx1_spec = GPIO_DT_SPEC_GET(BTN_MATRIX_1, gpios);  
+    static const struct gpio_dt_spec btnMtrx2_spec = GPIO_DT_SPEC_GET(BTN_MATRIX_2, gpios);  
+    static const struct gpio_dt_spec btnMtrx3_spec = GPIO_DT_SPEC_GET(BTN_MATRIX_3, gpios);  
+    static const struct gpio_dt_spec btnMtrx4_spec = GPIO_DT_SPEC_GET(BTN_MATRIX_4, gpios);  
+    static const struct gpio_dt_spec btnMtrx5_spec = GPIO_DT_SPEC_GET(BTN_MATRIX_5, gpios);  
+    static const struct gpio_dt_spec btnMtrx6_spec = GPIO_DT_SPEC_GET(BTN_MATRIX_6, gpios);  
 
-    keyMatrixOut[2].device = gpio0_dev;
-    keyMatrixOut[2].pinNum = 2;
+    keyMatrixOut[0] = &ledx0_spec;
+    keyMatrixOut[1] = &ledx1_spec;
+    keyMatrixOut[2] = &ledx2_spec;
+    keyMatrixOut[3] = &ledx3_spec;
+    keyMatrixOut[4] = &ledx4_spec;
+    keyMatrixOut[5] = &ledx5_spec;
+    keyMatrixOut[6] = &ledx6_spec;
+    keyMatrixOut[7] = &ledx7_spec;
 
-    keyMatrixOut[3].device = gpio0_dev;
-    keyMatrixOut[3].pinNum = 3;
+    testLED = &testLed_spec; 
 
-    keyMatrixOut[4].device = gpio0_dev;
-    keyMatrixOut[4].pinNum = 4;
-
-    keyMatrixOut[5].device = gpio0_dev;
-    keyMatrixOut[5].pinNum = 5;
-
-    keyMatrixOut[6].device = gpio0_dev;
-    keyMatrixOut[6].pinNum = 6;
-
-    keyMatrixOut[7].device = gpio0_dev;
-    keyMatrixOut[7].pinNum = 7;
-
-// // GPIO inputs 
-//     keyMatrixIn[0].device = gpio0_dev;
-//     keyMatrixIn[0].pinNum = 10; 
-
-//     keyMatrixIn[1].device = gpio1_dev;
-//     keyMatrixIn[1].pinNum = 10; 
-
-//     keyMatrixIn[2].device = gpio1_dev;
-//     keyMatrixIn[2].pinNum = 13; 
-
-//     keyMatrixIn[3].device = gpio1_dev;
-//     keyMatrixIn[3].pinNum = 15; 
-
-//     keyMatrixIn[4].device = gpio0_dev;
-//     keyMatrixIn[4].pinNum = 02; 
-
-//     keyMatrixIn[5].device = gpio0_dev;
-//     keyMatrixIn[5].pinNum = 29; 
-
-//     keyMatrixIn[6].device = gpio0_dev;
-//     keyMatrixIn[6].pinNum = 31; 
-
+    keyMatrixIn[0] = &btnMtrx0_spec; 
+    keyMatrixIn[1] = &btnMtrx1_spec; 
+    keyMatrixIn[2] = &btnMtrx2_spec; 
+    keyMatrixIn[3] = &btnMtrx3_spec; 
+    keyMatrixIn[4] = &btnMtrx4_spec; 
+    keyMatrixIn[5] = &btnMtrx5_spec; 
+    keyMatrixIn[6] = &btnMtrx6_spec; 
+    
+    gpio_pin_configure_dt(testLED, GPIO_OUTPUT); 
     for(uint16_t i = 0; i < KEY_MATRIX_OUT_COUNT; i++) {
-        gpio_pin_configure(keyMatrixOut[i].device, keyMatrixOut[i].pinNum, GPIO_OUTPUT  );
+        gpio_pin_configure_dt(keyMatrixOut[i], GPIO_OUTPUT);
     }
-    // for(uint17_t i = 0; i < KEY_MATRIX_IN_COUNT; i++) {
-    //     gpio_pin_configure(keyMatrixOut[i].device, keyMatrixOut[i].pinNum, GPIO_INPUT);
+
+    for(uint16_t i = 0; i < KEY_MATRIX_IN_COUNT; i++) {
+        gpio_pin_configure_dt(keyMatrixIn[i], GPIO_INPUT);
+    }
+
+
+    // for(uint16_t i = 0; i < KEY_MATRIX_OUT_COUNT; i++) {
+    //     gpio_pin_set_dt(keyMatrixOut[i], 1);
     // }
 }
 
-#define MY_LED_NODE        DT_NODELABEL(leds4)
-
-
 void keyMatrixTask() {
-    // initKeyMatrix(); 
-    // while(1) {
-    //     for(uint16_t i = 0; i < KEY_MATRIX_OUT_COUNT; i++){
-    //         gpio_pin_set(keyMatrixOut[i].device, keyMatrixOut[i].pinNum, 1); 
-    //         k_msleep(1000); 
-    //         gpio_pin_set(keyMatrixOut[i].device, keyMatrixOut[i].pinNum, 0); 
-    //     }
-    // }
+    initKeyMatrix(); 
+    int isPressed = 0; 
 
-    static const struct gpio_dt_spec myLed_spec = GPIO_DT_SPEC_GET(MY_LED_NODE, gpios);
-    gpio_pin_configure_dt(&myLed_spec, GPIO_OUTPUT); 
 
     while(1) {
-        gpio_pin_set_dt(&myLed_spec, 1); 
-        k_msleep(SLEEP_TIME_MS); 
-        gpio_pin_set_dt(&myLed_spec, 0); 
-        k_msleep(SLEEP_TIME_MS); 
+        for(uint16_t i = 0; i < KEY_MATRIX_OUT_COUNT; i++) {
+            gpio_pin_set_dt(keyMatrixOut[i], 1);
+            for(uint16_t j = 0; j < KEY_MATRIX_IN_COUNT; j++) {
+                isPressed = gpio_pin_get(keyMatrixIn[j]->port, keyMatrixIn[j]->pin);
+                if(isPressed) {
+                    gpio_pin_set_dt(testLED, 1); 
+                }
+            }
+            k_msleep(SLEEP_TIME_MS); 
+            gpio_pin_set_dt(keyMatrixOut[i], 0);
+            gpio_pin_set_dt(testLED, 0); 
+        }
     }
 }
--- 01_FW/src/keys.h
+++ 01_FW/src/keys.h
@@ -1,5 +1,16 @@
 #ifndef KEYS
 #define KEYS
 
+typedef enum {
+    KEY_PRESSED, 
+    KEY_RELEASED
+} keyState_e; 
+
+typedef struct _keyBrdKey_ {
+    keyState_e state; 
+    keyState_e statePrev; 
+    uint8_t scanCode; 
+} KB_key_t;
+
 void keyMatrixTask(); 
 #endif
